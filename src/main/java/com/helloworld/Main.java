00c903a9b55323c2917c74108ce85a15de3dba92
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-07-31 16:05:35 +0500

    git ignore changed

--- src/main/java/com/helloworld/Main.java
+++ src/main/java/com/helloworld/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        for (String i:args
+             ) {
+            System.out.println(i);
+        }
         System.out.println("Hello world! test merge" + "THIS IS A NEW CONTENT IN MATER BRANCH");
         System.out.println("master branch");
         System.out.println("тест");
